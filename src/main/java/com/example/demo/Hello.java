15c646174bdaf6d102b2db1d273c7608a2d3a9c8
moni0012 <moni.kumari@db.com>
2022-06-22 14:37:42 +0300

    add getter and setter

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -6,6 +6,30 @@ import javax.persistence.Id;
 @Entity
 public class Hello {
 
+    public int getId() {
+        return id;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    @Override
+    public String toString() {
+        return "Hello{" +
+                "id=" + id +
+                ", greeting='" + greeting + '\'' +
+                '}';
+    }
+
     @Id
     private int id;
 
